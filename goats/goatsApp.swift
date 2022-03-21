1da75210b9d62fa1c1fe6ff397d9df30312e331a
kwe <kwevans@gmail.com>
2022-03-21 15:54:44 +0000

    Initial Commit

---
+++ goats/goatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  goatsApp.swift
+//  goats
+//
+//  Created by Kevin W Evans on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct goatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
